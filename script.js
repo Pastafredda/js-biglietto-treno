e0fdafd653cc74451d292c930466ab88c5c213dd
Pastafredda <justspotify09@gmail.com>
2023-04-27 22:15:57 +0200

    fine

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Dichiarazione di variabili
-let numeroKm , eta , prezzoBiglietto, sconto, prezzoFinale, messaggio;
+let numeroKm , eta , prezzoBiglietto, sconto, prezzoFinale, messaggio, fixed;
+const cifra= "€";
 
 // Chiediamo quanti chilometri deve percorrere 
 numeroKm = prompt ("Inserisci il numero di chilometri da percorrere");
@@ -28,10 +29,14 @@ prezzoBiglietto = numeroKm * 0.21 ;
 console.log(prezzoBiglietto);
 
 // Sconto
-bigliettoSconto = prezzoBiglietto * sconto / 100 ;
+bigliettoSconto = Math.round(prezzoBiglietto * sconto / 100)  ;
 prezzoFinale = prezzoBiglietto - bigliettoSconto ;
+fixed=prezzoFinale.toFixed(2);
 console.log(bigliettoSconto);
 console.log(prezzoFinale);
+console.log(fixed);
+
+messaggio= `Il tuo biglietto costa ${fixed+cifra}`;
 
 document.getElementById("ticket").innerHTML= messaggio
 
